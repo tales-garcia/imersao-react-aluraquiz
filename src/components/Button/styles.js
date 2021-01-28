052e8bb9531c67903f22cfc6215a2401808f1891
Tales Garcia <garcia.tales@gmail.com>
2021-01-28 17:02:11 -0300

    feat(components): create button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border: 0;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  color: ${({ theme }) => theme.colors.contrastText};
+  padding: 10px 20px;
+  font-size: 18px;
+  font-weight:  500;
+  font-family: 'Lato', serif;
+  cursor: pointer;
+
+  &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+  }
+`;
