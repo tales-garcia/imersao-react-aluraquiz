d1f622a98bd4fdd6508abd80a1e2f4879234aa1c
Tales Garcia <garcia.tales@gmail.com>
2021-01-27 22:30:34 -0300

    feat(main page): implement page structure

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Footer from '../src/components/Footer';
 import QuizBackground from '../src/components/QuizBackground';
 import GithubCorner from '../src/components/GithubCorner';
 import Widget from '../src/components/Widget';
+import { useRouter } from 'next/router';
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -17,18 +18,36 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const [name, setName] = React.useState();
+  const router = useRouter();
+
+  const handleSubmit = React.useCallback(e => {
+    e.preventDefault();
+
+    router.push(`/quiz?nickname=${name}`);
+  });
+
   return (
     <QuizBackground backgroundImage={db.bg} >
       <QuizContainer>
-        <Widget title="The legend of Zelda">
-          <p>Lorem ipsum dolor sit amet...</p>
+        <Widget title={db.title}>
+          <form onSubmit={handleSubmit}>
+            <input
+              placeholder="Nome"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+            <button disabled={!name} type="submit">
+              Jogar
+            </button>
+          </form>
         </Widget>
         <Widget>
           <h1>Quizes da galera</h1>
           <p>Lorem ipsum dolor sit amet...</p>
         </Widget>
+        <Footer />
       </QuizContainer>
-      <Footer />
       <GithubCorner />
     </QuizBackground>
   )
