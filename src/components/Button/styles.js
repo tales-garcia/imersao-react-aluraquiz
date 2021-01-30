20873391fe9aaa74d06294f0169d27883041f41c
Tales Garcia <garcia.tales@gmail.com>
2021-01-30 16:25:14 -0300

    refactor(button styles): import motion

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const Container = styled.button`
+export const Container = styled(motion.button)`
   width: 100%;
   background-color: ${({ theme }) => theme.colors.secondary};
   border: 0;
