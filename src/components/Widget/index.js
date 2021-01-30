baed3c453ab3c3cd7ce412f80bb8c76eeb5a76f3
Tales Garcia <garcia.tales@gmail.com>
2021-01-30 16:26:28 -0300

    feat(widget component): add animations to widget

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Container } from './styles';
+import { motion } from 'framer-motion';
 
-export default function Widget({ title, children, image }) {
+export default function Widget({ title, children, image, ...rest }) {
     return (
-        <Container>
+        <Container
+            transition={{ delay: 0, duration: 0.4 }}
+            as={motion.section}
+            variants={{
+                shown: { opacity: 1, x: '0' },
+                hidden: { opacity: 0, x: '-20%' }
+            }}
+            initial="hidden"
+            animate="shown"
+            {...rest}
+        >
             {title &&
             (<Container.Header>
                 <h1>{title}</h1>
