c8069f27dcdd9bdc761260aa0af9934b9658b4eb
Tales Garcia <garcia.tales@gmail.com>
2021-01-30 16:25:39 -0300

    feat(button component): add hover and tap animations to button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import { Container } from './styles';
 
 export default function Button({ children, ...rest }) {
-    return <Container {...rest}>{children}</Container>
+    return (
+        <Container
+            whileHover={{ scale: 1.01 }}
+            whileTap={{ scale: 0.99 }}
+            {...rest}
+        >
+            {children}
+        </Container>
+    );
 }
