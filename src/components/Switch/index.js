525074e84a2be86bb00b5a748c56ed96e013a1e6
Tales Garcia <garcia.tales@gmail.com>
2021-01-30 16:30:41 -0300

    feat(switch component): add animations to switch

--- src/components/Switch/index.js
+++ src/components/Switch/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from '../Button';
 import { Container, Option } from './styles';
+import { motion, useAnimation } from 'framer-motion';
 
-export default function Switch({ alternatives, selected, setSelected, correct, goToNextQuestion }) {
+export default function Switch({ alternatives, selected, setSelected, correct, goToNextQuestion, ...rest }) {
     const [submited, setSubmited] = React.useState(false);
+    const control = useAnimation();
 
     const handleSubmit = React.useCallback(async () => {
         setSubmited(true);
@@ -18,9 +20,14 @@ export default function Switch({ alternatives, selected, setSelected, correct, g
         return new Promise(resolve => setTimeout(resolve, ms));
     }, [])
 
+    React.useEffect(() => {
+        control.set('hidden');
+        control.start('shown');
+    }, [alternatives]);
+
     return (
         <>
-            <Container>
+            <Container {...rest}>
                 {alternatives.map((alternative, index) => (
                     <Option
                         key={index}
@@ -28,6 +35,21 @@ export default function Switch({ alternatives, selected, setSelected, correct, g
                         active={selected === index}
                         wrong={selected === index && submited && selected !== correct}
                         correct={selected === index && submited && selected === correct}
+                        custom={index}
+                        as={motion.li}
+                        variants={{
+                            shown: custom => ({
+                                opacity: 1,
+                                visibility: 'unset',
+                                x: '0',
+                                transition: { delay: (custom + 3.1) * 0.2, duration: 0.4 }
+                            }),
+                            hidden: { visibility: 'hidden', opacity: 0, x: '-35px' }
+                        }}
+                        whileHover={{ scale: 1.06 }}
+                        whileTap={{ scale: 1.03 }}
+                        initial="hidden"
+                        animate={control}
                     >
                         {alternative}
                     </Option>
