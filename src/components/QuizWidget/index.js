f523a4f5eb2bf05059bb23780dd16c9feee8ad41
Tales Garcia <garcia.tales@gmail.com>
2021-01-30 16:32:42 -0300

    feat(quiz widget): add animations to quiz widget

--- src/components/QuizWidget/index.js
+++ src/components/QuizWidget/index.js
@@ -3,6 +3,8 @@ import Switch from '../../../src/components/Switch';
 import Widget from '../../../src/components/Widget';
 import styled from 'styled-components';
 import BackLinkArrow from '../BackLinkArrow';
+import { AnimateSharedLayout, motion, useAnimation } from 'framer-motion';
+import Result from '../Result';
 
 const GoBack = styled(BackLinkArrow)`
   margin-right: 16px;
@@ -10,25 +12,10 @@ const GoBack = styled(BackLinkArrow)`
   text-align: center;
 `;
 
-const Result = styled.li`
-    color: ${({ theme }) => theme.colors.contrastText};
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px 20px;
-    border-radius: ${({ theme }) => theme.borderRadius};
-    background-color: ${({ theme }) => theme.colors.primary}70;
-    border: 2px solid ${({ theme, questionResult }) => questionResult ? theme.colors.success : theme.colors.wrong};
-
-    & + li {
-        margin-top: 8px;
-    }
-
-    > span {
-        color: ${({ theme, questionResult }) => questionResult ? theme.colors.success : theme.colors.wrong};
-        font-weight: 600;
-    }
+const NextQuestion = styled(motion.div)`
+    position: absolute;
+    top: 24px;
+    width: calc(100% - 64px);
 `;
 
 export default function QuizWidget({ db }) {
@@ -36,16 +23,37 @@ export default function QuizWidget({ db }) {
     const [loading, setLoading] = React.useState(true);
     const [question, setQuestion] = React.useState(0);
     const [results, setResults] = React.useState([]);
+    const questionControl = useAnimation();
+    const ref = React.useRef(null);
+    const ref2 = React.useRef(null);
 
-    const goToNextQuestion = React.useCallback(() => {
-        setResults([...results, db.questions[question].answer === selected]);
+    const goToNextQuestion = React.useCallback(async () => {
+        await questionControl.start('go');
+        questionControl.set('come');
+
+        setResults([...results, {
+            correct: db.questions[question].answer === selected,
+            anwser: selected
+        }]);
         setQuestion(question + 1);
         setSelected(undefined);
+
+        if (ref2.current) {
+            ref2.current.style.opacity = 1;
+            ref2.current.style.transform = 'unset';
+        }
+        if (ref.current) {
+            ref.current.style.visibility = 'hidden';
+        }
     }, [question, selected, setQuestion, setSelected, setResults]);
 
     React.useEffect(() => {
-        setTimeout(() => setLoading(false), 100);
+        setTimeout(() => setLoading(false), 2000);
     }, []);
+
+    React.useEffect(() => {
+        questionControl.set('come');
+    }, [question]);
 
     return loading ? (
         <Widget title="Carregando...">
@@ -69,26 +77,66 @@ export default function QuizWidget({ db }) {
             >
                 {question !== db.questions.length ? (
                     <>
-                        <h3>{db.questions[question].title}</h3>
-                        <p>{db.questions[question].description}</p>
-
-                        <Switch
-                            alternatives={db.questions[question].alternatives}
-                            correct={db.questions[question].answer}
-                            selected={selected}
-                            setSelected={setSelected}
-                            goToNextQuestion={goToNextQuestion}
-                        />
+                        <motion.div
+                            variants={{
+                                come: { x: '0%', opacity: 1 },
+                                right: { x: '100%', opacity: 0 },
+                                go: { x: '-100%', opacity: 0 }
+                            }}
+                            initial={!!question && "right"}
+                            animate={questionControl}
+                            ref={ref2}
+                            transition={{ delay: 0.3, duration: 0.6 }}
+                        >
+                            <h3>{db.questions[question].title}</h3>
+                            <p>{db.questions[question].description}</p>
+                            <Switch
+                                alternatives={db.questions[question].alternatives}
+                                correct={db.questions[question].answer}
+                                selected={selected}
+                                setSelected={setSelected}
+                                goToNextQuestion={goToNextQuestion}
+                            />
+                        </motion.div>
+                        {!!db.questions[question + 1] && (
+                            <NextQuestion
+                                variants={{
+                                    come: { x: 'calc(100% + 32px)', opacity: 0 },
+                                    right: { x: '200%', opacity: 0 },
+                                    go: { x: '0%', opacity: 1, visibility: 'unset' }
+                                }}
+                                ref={ref}
+                                initial="right"
+                                animate={questionControl}
+                                transition={{ delay: 0.3, duration: 0.6 }}
+                            >
+                                <h3>{db.questions[question + 1].title}</h3>
+                                <p>{db.questions[question + 1].description}</p>
+                                <Switch
+                                    alternatives={db.questions[question + 1].alternatives}
+                                    correct={db.questions[question + 1].answer}
+                                    selected={undefined}
+                                    style={{ visibility: 'hidden' }}
+                                    setSelected={setSelected}
+                                    goToNextQuestion={goToNextQuestion}
+                                />
+                            </NextQuestion>
+                        )}
                     </>
                 ) : (
                         <>
                             <p>Você acertou {results.filter(result => result).length} de {db.questions.length} questões!</p>
                             <ul>
-                                {results.map((result, index) => (
-                                    <Result questionResult={result} key={index}>
-                                        Pergunta {index + 1} <span>{result ? 'Acertou' : 'Errou'}</span>
-                                    </Result>
-                                ))}
+                                <AnimateSharedLayout>
+                                    {results.map((result, index) => (
+                                        <Result
+                                            data={result}
+                                            key={index}
+                                            questionNumber={index}
+                                            question={db.questions[index]}
+                                        />
+                                    ))}
+                                </AnimateSharedLayout>
                             </ul>
                         </>
                     )}
